dec6b45f198a0a770fa8491707eb1298c7e9d3d9
projeta618 <projeta618@gmail.com>
2023-10-14 21:15:51 -0300

    Repair merge.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 
-from kernel.http import Response
+from django.shortcuts import render
+from kernel.http import Response 
 from .libs import create_token as create_token_lib
 
 def create_token(request):
@@ -18,4 +19,25 @@ def create_token(request):
         relatedModelId=relatedModelId
     )
     res.token = dbToken.serialize(request)
+    return res.success()
+
+def token_exists(request):
+    """
+        @description: Verify if the token exists. 
+    """
+    res = Response(request=request)
+    return res.success()
+
+def use_token(request):
+    """
+        @description: Use the token and delete it from the database
+    """
+    res = Response(request=request)
+    return res.success()
+
+def create_token(request):
+    """
+        @description: Create a token
+    """
+    res = Response(request=request)
     return res.success()
