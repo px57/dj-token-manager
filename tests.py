3188419756b0a442e1f76c955f0f492dca5df8da
projeta618 <projeta618@gmail.com>
2023-10-25 15:33:15 -0300

    Update the element

--- tests.py
+++ tests.py
@@ -47,7 +47,6 @@ class RedirectWithToken(TestCase):
             relatedModelId=1
         )
         redirect_url = token.create_redirect_url(res)
-        print (redirect_url)
 
     def test_use_redirect_url(self):
         """
@@ -60,8 +59,5 @@ class RedirectWithToken(TestCase):
             relatedModelId=1
         )
         redirect_url = token.create_redirect_url(res)
-        print ('--------------------------------------')
-        print (redirect_url)
         c = Client()
         response = c.get(redirect_url)
-        print (response.content)
